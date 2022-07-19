ca75792c36f15cf7f18c0031e405e89e54cb6275
Philip I. Thomas <mail@philipithomas.com>
2022-07-19 15:12:35 -0700

    formatting fixes

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="pt-20 overflow-hidden text-gray-100 bg-gray-950 gray-100 font-body"
+  >
+    <div class="min-h-screen">
+      <main class="container bg-gray-500">
+        <nuxt />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
