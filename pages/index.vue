714a2a64695dffca40641d37bacead9ee1417851
Philip I. Thomas <mail@philipithomas.com>
2022-08-03 09:55:10 -0500

    add basic homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Product development studio by Philip Thomas</div>
+  <div class="flex-grow flex flex-col justify-center max-w-md">
+    <h1 class="text-4xl md:text-7xl font-semibold text-gray-050">
+      Product development studio
+    </h1>
+    <h2 class="text-lg pt-4 md:pt-8 text-gray-075 uppercase">
+      By
+      <a
+        href="https://philipithomas.com"
+        target="_blank"
+        class="accent-underline"
+      >Philip I. Thomas</a>
+    </h2>
+  </div>
 </template>
 
 <script>
